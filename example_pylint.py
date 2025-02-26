03cb6cb2c1b741c7b19182a26b1d1ce7fee8547e
Fernando Sartor <fernandosartor@outlook.com>
2025-02-26 05:57:39 -0300

    config: add init to project

--- example_pylint.py
+++ example_pylint.py
@@ -1,9 +1,10 @@
 print("Olá mundo")
 
-def minha_Funcao():
+def minha_funcao():
     ''' Alguma informação '''
     print("Estou na função")
 
 # snake_case -> Variáveis e funções em Python
 # cammelCase -> Não consta
 # PascalCase -> Classes em Python
+# pre-commit run --all-files
