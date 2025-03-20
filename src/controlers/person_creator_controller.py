8097a307e0abce0063c5893d316064a176d05674
Fernando Sartor <fernandosartor@outlook.com>
2025-03-20 17:48:44 -0300

    feat: Implementing person_creator_controller with tests

--- src/controlers/person_creator_controller.py
+++
@@ -1,7 +0,0 @@
-from src.models.sqlite.interfaces.people_repository import PeopleRepostoryInterface
-
-class PersonCreatorController:
-    # Inversão da dependência: Associa models com controlers
-    def __init__(self, people_repository: PeopleRepostoryInterface) -> None: 
-        self.__people_repository = people_repository
-        
