19da44f33e20e23113633d050bd3ab823ba112fc
Fernando Sartor <fernandosartor@outlook.com>
2025-04-07 14:21:18 -0300

    feat: Implementing error handle

--- src/errors/error_types/http_unprocessable_entity.py
+++ src/errors/error_types/http_unprocessable_entity.py
@@ -1,7 +1,7 @@
 class HttpUnprocessableentityError(Exception):
     
     def __init__(self, message: str) -> None:
-        super.__init__(message)
+        super().__init__(message)
         self.status_code = 422
         self.name = 'UnprocessableEntity'
         self.message = message
